6b0270b2e5fc02b50f78dbe0c3e6b2066c27b094
keanamaral <keanam@yahoo.com>
2018-05-20 11:40:36 -0400

    adding files

---
+++ app/data/friends.js
@@ -0,0 +1,169 @@
+// ===============================================================================
+// DATA
+// Storing all the friends data below
+// ===============================================================================
+var friendsArray = [
+	{
+		name: 'Bird',
+		photo: 'https://www.corbettmachaanresort.com/images/12/wildlife12.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Wild Cat',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Winter-Tiger-Wild-Cat-Images.jpg/1280px-Winter-Tiger-Wild-Cat-Images.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Marathi Bud',
+		photo: 'https://www.marathi.tv/wp-content/uploads/2015/06/bhau-kadam-marathi-actor.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Kevin Bacon',
+		photo: 'https://cbscw44.files.wordpress.com/2013/02/1406351381.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Dr. Evil',
+		photo: 'https://cdn-images-1.medium.com/max/800/1*ZYpBSAe0dC4_ha-3GhcO9Q.jpeg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Geico Gecko',
+		photo: 'https://images.forbes.com/media/2011/03/08/0308_beloved-spokescreatures-geico-gecko_485x340.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Johnny Depp',
+		photo: 'https://moviesdrop.com/wp-content/uploads/2013/09/Johnny-Depp-Facts.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Adele Adkins',
+		photo: 'https://i1.imgiz.com/data/videoshots/6829/6829475-6461.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Kermit the Frog',
+		photo: 'http://i.dailymail.co.uk/i/pix/2015/09/02/08/2BE5406100000578-3218820-image-m-25_1441179775589.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Elmo',
+		photo: 'http://1rjm5qg4v8732kpmn44ya6bj-wpengine.netdna-ssl.com/wp-content/uploads/2012/11/elmo-6601.png',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+// Exporting friends array
+module.exports = friendsArray;
